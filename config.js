408f76657a0d490db80ec790c92abc3968304637
KhamidBatang <111238475+KhamidBatang@users.noreply.github.com>
2022-08-31 15:48:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,11 +14,11 @@
 gc1 = ''
 gc2 = ''
 gc3 = ''
-global.linkGC = ['https://chat.whatsapp.com/HJ9pD2XJ8vq7whuAYOvT1Y', ''] // ganti jadi group lu
-global.owner = ['085725924473'] // Put your number here //owner eval
-global.kontak = ['085725924473','0'] //Ketika ada yang ngetik #owner
+global.linkGC = [' ', ''] // ganti jadi group lu
+global.owner = ['081947707409'] // Put your number here //owner eval
+global.kontak = ['081947707409','0'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = ['085725924473'] // Premium user has unlimited limit
+global.prems = ['081947707409'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -47,20 +47,20 @@ xteamkey = 'Dawnfrostkey'
 namaig = 'GAK ADA :)'
 namagithub = 'GAK ADA :)'
 kasihcaption = `Nih kak`
-namakontak1 = 'KAKA BOT'
-namakontak2 = 'KAKA BOT'
+namakontak1 = 'REX-MD'
+namakontak2 = 'REX-MD'
 
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'By KAKA BOT' // ganti aja
-global.author = 'KAKA BOT' // ganti aja
+global.packname = 'By REX-MD' // ganti aja
+global.author = 'REX-MD' // ganti aja
 
 //yyy
 bc = 'AlyaaXzy' //KAKA BOT Broadcast
-footer = '\n©KAKA BOT'
-namabot = 'KAKA BOT'
-namalu = 'KAKA BOT'
+footer = '\n©REX-MD '
+namabot = 'REX-MD'
+namalu = 'REX-MD'
 
 
 // 
